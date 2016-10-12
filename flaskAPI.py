f7f8404ddd474ec6fa390927fc51b0095c18652c
thevoncarl <viktor.palerius@gmail.com>
2016-10-12 17:46:23 +0200

    Added a progress bar on the front end, and integrated it into the restAPI.

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,9 +1,80 @@
 #!flask/bin/python
 # coding=utf-8
-from flask import Flask, jsonify, render_template, request 
-from tasks import generateMesh
+from flask import Flask, jsonify, render_template, request, url_for
+import time
+from threading import Thread
+#from tasks import generateMesh
 
 app = Flask(__name__)
+response_airfoil = {}
+nr_msh_job_finished = 0;
+nr_xml_job_finished = 0;
+nr_airfoil_job_finished = 0;
+
+total_nr_of_jobs = 30;
+
+
+## This function shows how an example for the progress bar
+#TODO: Delete this function
+def threaded_function():
+    x = 0
+    y = 0
+    z = 0
+    while(x < 100):
+        x += 10
+        updateResponse("mesh")
+        
+    
+        time.sleep(1)
+
+    while(y < 100):
+        y += 10
+        updateResponse("xml")
+            
+    
+        time.sleep(1)
+
+    while(z < 100):
+        z += 10
+        updateResponse("airfoil")
+           
+    
+        time.sleep(1)
+
+def updateResponse(jobFinished):
+    global response_airfoil
+    global nr_msh_job_finished
+    global nr_xml_job_finished
+    global nr_airfoil_job_finished
+    global total_nr_of_jobs
+    nr_job_finished = nr_msh_job_finished +  nr_xml_job_finished + nr_airfoil_job_finished
+    
+    if jobFinished == "mesh":
+        nr_msh_job_finished += 1
+        
+        response_airfoil = {
+            'current': nr_job_finished + 1,
+            'total': total_nr_of_jobs,
+            'status': "Generating mesh files..."
+        }
+
+    if jobFinished == "xml":
+        nr_xml_job_finished += 1
+        
+        response_airfoil = {
+            'current': nr_job_finished + 1,
+            'total': total_nr_of_jobs,
+            'status': "Converting to XML..."
+        }
+
+    if jobFinished == "airfoil":
+        nr_airfoil_job_finished += 1
+        
+        response_airfoil = {
+            'current': nr_job_finished + 1,
+            'total': total_nr_of_jobs,
+            'status': "Running airfoil..."
+        }
 
 
 @app.route('/', methods=['GET'])
@@ -19,9 +90,22 @@ def data_post():
     n_levels = request.form['n_levels']
     
     processed_text = angle_start.upper()
-    return processed_text
- 
 
+    # TODO:THIS IS WHERE WE RUN THE startProcess Thread
+    return render_template('status.html')
+
+
+##These functions are constantly pulled by jquery script in status.html #######
+@app.route('/status_airfoil')
+def status_airfoil():
+     return jsonify(response_airfoil)
+
+@app.route('/airfoil', methods=['POST'])
+def airfoil():
+    
+     return jsonify({}), 202, {'Location': url_for('status_airfoil')}
+ 
+###############################################################################
 
 
 def startProcess(angle_start, angle_stop, n_angles, n_nodes, n_levels):
@@ -30,7 +114,8 @@ def startProcess(angle_start, angle_stop, n_angles, n_nodes, n_levels):
     NACA2 = 0
     NACA3 = 1
     NACA4 = 2
-
+    
+    #TODO: Calculate total amount of jobs to be done
     ##START OF MESH TASKS
     
     angleDiff = (angle_stop-angle_start)/n_angles
@@ -56,7 +141,11 @@ def startProcess(angle_start, angle_stop, n_angles, n_nodes, n_levels):
 
 
 if __name__ == '__main__':
-    
+
+    ## TODO: Delete line with thread
+    thread = Thread(target = threaded_function)
+    thread.start()
+
     app.run(host='0.0.0.0',debug=True)
 
 
